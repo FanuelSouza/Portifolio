2112f4f90e843f5f47655dfc7ef3fb14570de07b
FanuelSouza <36958247+FanuelSouza@users.noreply.github.com>
2018-03-01 10:37:51 -0300

    Inserindo o banner de fundo

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+		<title>Fanuel Souza</title>
+		<link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+</head>
+
+<body>
+
+	<header>
+		<div id="logobranca"><img src="IMG/logo.png"></div>
+		<div id="bannertopo"><img src=""></div>
+		
+		<nav>
+			<div id="Menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Portifólio</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</div>
+
+			<div id="icones">
+				
+			</div>
+
+		</nav>
+
+	</header>
+
+</body>
+
+
+</html>
