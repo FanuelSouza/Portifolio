db6117efa7dad64c8966000dc29b19d923079fd2
FanuelSouza <36958247+FanuelSouza@users.noreply.github.com>
2018-03-02 11:15:56 -0300

    Cabeçario página inicial está pronto!
    Tanto o HTML quanto CSS estão prontos, embora o efeito ":HOVER" ainda não esteja inserido.

--- index.php
+++ index.php
@@ -1,33 +1,47 @@
 <html>
 
 <head>
+		<meta charset="utf-8">
 		<title>Fanuel Souza</title>
 		<link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+ 		<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+ 		<script defer src="fontawesome-free-5.0.7/svg-with-js/js/fontawesome-all.js"></script>
+ 		<script defer src="fontawesome-free-5.0.7/svg-with-js/js/fa-v4-shims.js"></script>
 </head>
 
 <body>
 
 	<header>
-		<div id="logobranca"><img src="IMG/logo.png"></div>
-		<div id="bannertopo"><img src=""></div>
+
+		<div id="bannertopo"><img src="IMG/topoindex.jpg"></div>
+		<div id="logobranca"><a href="index.php"><img src="IMG/logobranca.png" width="40"></a></div>
+		<div id="barra"><img src="IMG/barraindex.png" width="100%" height="30px"></div>
 		
 		<nav>
 			<div id="Menu">
 				<ul>
 					<li><a href="index.php">Home</a></li>
-					<li><a href="">Sobre</a></li>
-					<li><a href="">Portifólio</a></li>
-					<li><a href="">Contato</a></li>
+					<li><a href="sobre.php">Sobre</a></li>
+					<li><a href="portifolio.php">Portifólio</a></li>
+					<li><a href="contato.php">Contato</a></li>
 				</ul>
 			</div>
-
-			<div id="icones">
-				
-			</div>
-
 		</nav>
 
+		<ul class="social">
+			<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+			<li><a href="#"><i class="fab fa-github"></i></a></li>
+		</ul>
+
+
+		<h1><strong>Fanuel</strong> C. <strong>Souza</strong></h1>
+		<p> Sites // Banners</p>
+		
+
 	</header>
+		
+
+	
 
 </body>
 
